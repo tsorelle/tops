63c33b82ae1e5566d9745ff4b62f6965a366e1e6
Terry SoRelle <tls@2quakers.net>
2017-07-03 06:51:58 -0500

    Tested injected object container

---
+++ tests/lib/TestObjectContainer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry
+ * Date: 7/3/2017
+ * Time: 6:45 AM
+ */
+
+namespace TwoQuakers\testing;
+
+
+use Tops\sys\TSimpleObjectContainer;
+
+class TestObjectContainer extends TSimpleObjectContainer
+{
+
+}
