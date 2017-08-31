0009836fd42dc03ae4ae2907db5db99ff86d2a50
Terry SoRelle <tls@2quakers.net>
2017-08-31 09:58:58 -0500

    fixes

--- tops/lib/mail/TDbMailboxManager.php
+++ tops/lib/mail/TDbMailboxManager.php
@@ -48,7 +48,7 @@ namespace Tops\mail;
       */
      public function findByCode($mailboxCode)
      {
-         $result = $this->repository->getFirst("code='$mailboxCode'");
+         $result = $this->repository->getFirst("mailboxcode='$mailboxCode'");
          if (empty($result)) {
              return false;
          }
