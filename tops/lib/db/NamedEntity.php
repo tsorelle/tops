2c8ad06162767d54bff070a82fd4d4eb1f124eb7
Terry SoRelle <tls@2quakers.net>
2017-10-28 15:56:38 -0500

    added NamedEntity

---
+++ tops/lib/db/NamedEntity.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry
+ * Date: 10/28/2017
+ * Time: 3:13 PM
+ */
+
+namespace Tops\db;
+
+
+class NamedEntity extends TimeStampedEntity
+{
+    public $name = '';
+    public $code = '';
+    public $description = '';
+
+    public function setName($value)
+    {
+        if (empty($value)) {
+            throw new \Exception('The name field cannot be blank.');
+        }
+        $this->name = $value;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function setCode($value)
+    {
+        if (empty($value)) {
+            throw new \Exception('The code field cannot be blank.');
+        }
+        $this->code = $value;
+    }
+
+    public function setDescription($value) {
+        $this->description = empty($value) ? '' : $value;
+    }
+
+    public function setValues($name,$code,$description) {
+        $this->setName($name);
+        $this->setCode($code);
+        $this->setDescription($description);
+    }
+
+}
