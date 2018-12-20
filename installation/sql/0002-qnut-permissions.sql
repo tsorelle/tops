629305f3e4413b8e6cfb8b42087fd2cbfe84727e
Terry SoRelle <tls@2quakers.net>
2018-12-20 10:47:00 -0600

    Added distribution script for qnut

---
+++ installation/sql/0002-qnut-permissions.sql
@@ -0,0 +1,7 @@
+INSERT  INTO `tops_permissions`(`permissionName`,`description`,`active`) VALUES 
+('administer-directory','Administer directory',1),
+('view-directory','View directory',1),
+('update-directory','Update directory',1),
+('send-mailings','Send mailings',1),
+('edit-content','Edit content',1),
+('update-calendar','Update events calendar',1);
